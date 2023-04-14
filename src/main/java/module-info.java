7e002fdf96226f88893defa56b0cef90503cff15
Miquel Stam <mql.stam@gmail.com>
2023-04-14 19:35:07 +0200

    module-info updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,22 @@
 module com.codecademy {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.codecademy to javafx.fxml;
+    opens com.codecademy.controllers to javafx.fxml;
+    opens com.codecademy.dao to javafx.fxml;
+    opens com.codecademy.database to javafx.fxml;
+    opens com.codecademy.domain to javafx.fxml;
+
     exports com.codecademy;
+    exports com.codecademy.controllers;
+    exports com.codecademy.dao;
+    exports com.codecademy.database;
+    exports com.codecademy.domain;
+    
+
+
 }
+
+
