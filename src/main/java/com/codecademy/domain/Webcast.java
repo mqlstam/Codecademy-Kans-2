a0a488272ffaa4be57cea3840c16e8f65394ed8f
kgithcoy <anhtuannguyen.2004@gmail.com>
2023-04-16 19:00:53 +0200

    added new attribute to webcast domain class

--- src/main/java/com/codecademy/domain/Webcast.java
+++ src/main/java/com/codecademy/domain/Webcast.java
@@ -1,13 +1,14 @@
 package com.codecademy.domain;
 
 public class Webcast {
-    private String titel, description, hostName, organisationName, url;
+    private String titel, description, hostName, organisationName, speakername, url;
 
-    public Webcast(String titel, String description, String hostName, String organisationName, String url) {
+    public Webcast(String titel, String description, String hostName, String organisationName, String speakername, String url) {
         this.titel = titel;
         this.description = description;
         this.hostName = hostName;
         this.organisationName = organisationName;
         this.url = url;
+        this.speakername = speakername;
     }
 }
